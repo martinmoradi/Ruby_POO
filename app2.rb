4a8e1e67f1e43c31a4ca01ca3b21345acb050dd6
Martin Moradi <moradi.martin@gmail.com>
2020-10-13 21:00:57 +0200

    Exo 2 refacto

--- app2.rb
+++ app2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler'
 Bundler.require
 
@@ -7,29 +9,28 @@ require_relative 'lib/player'
 def human_battle
   welcome
   user1 = initialize_player
-  enemies = initialize_enemies 
+  enemies = initialize_enemies
 
   # ----------------COMBAT---------------------#
-  while user1.life_points > 0 && enemies.length > 0
+  while user1.life_points.positive? && enemies.length.positive?
     enemies = combat_attack(user1, *enemies)
     user1 = combat_defense(user1, *enemies)
     puts
   end
 
   # ---------------FIN DU GAME-----------------#
-  if user1.life_points <= 0 
+  if user1.life_points <= 0
     puts "#{user1.name} a perdu ! LOOOSER !"
-  else 
+  else
     puts "#{user1.name} a gagné ! Quel talent !"
   end
-
 end
 
-def welcome  
-  width = 120  
+def welcome
+  width = 120
   r1 = "Bienvenue sur 'ILS VEULENT TOUS MA POO' !"
   r2 = "Le but du jeu est d'être le dernier survivant !"
-  r3 = "=" * 120
+  r3 = '=' * 120
   puts r3
   puts r1.center(width)
   puts r2.center(width)
@@ -37,71 +38,68 @@ def welcome
 end
 
 def initialize_player
-  puts "Rentre ton pseudo :"
-  print "> "
+  puts 'Rentre ton pseudo :'
+  print '> '
   player_name = gets.chomp
-  user1 = HumanPlayer.new(player_name) 
-end 
+  user1 = HumanPlayer.new(player_name)
+end
 
-def initialize_enemies 
+def initialize_enemies
   enemies = []
-  bot1 = Player.new("Josianne")
-  bot2 = Player.new("José")
-  enemies << bot1 
+  bot1 = Player.new('Josianne')
+  bot2 = Player.new('José')
+  enemies << bot1
   enemies << bot2
-end 
+end
 
 def show_states(user1, *enemies)
   user1.show_state
-  enemies.each { |bot| bot.show_state }
+  enemies.each(&:show_state)
 end
 
 def combat_attack(user1, *enemies)
-  puts "-------------------------------"
+  puts '-------------------------------'
   user1.show_state
   combat_menu(*enemies)
   enemies = combat_input(user1, *enemies)
+  #---------Supprimer les bots morts de l'array ----#
   enemies.reject! { |bot| bot.life_points <= 0 }
-  enemies.compact  
+  enemies.compact
 end
 
 def combat_defense(user1, *enemies)
-  puts 
-  puts "Les ennemies attaquent !"
+  puts
+  puts 'Les ennemies attaquent !'
   enemies.each { |bot| bot.attacks(user1) }
   user1
 end
 
 def combat_menu(*enemies)
-  puts "Quelle action veux-tu effecter ?"
+  puts 'Quelle action veux-tu effecter ?'
   puts
-  puts "a - chercher une meilleure arme"
-  puts "s - chercher à se soigner"
+  puts 'a - chercher une meilleure arme'
+  puts 's - chercher à se soigner'
   puts
-  puts "attaquer un ennemi en vue :"
+  puts 'attaquer un ennemi en vue :'
   enemies.each.with_index do |bot, idx|
-    puts "#{idx}" + "- #{bot.show_state}" 
+    puts idx.to_s + "- #{bot.show_state}"
   end
-  puts "-------------------------------"
-  print "> "
+  puts '-------------------------------'
+  print '> '
 end
 
 def combat_input(user1, *enemies)
   input = gets.chomp
-  if input.downcase == "a"
+  if input.downcase == 'a'
     user1.search_weapon
-  elsif input.downcase == "s"
+  elsif input.downcase == 's'
     user1.search_health_pack
   elsif (0...enemies.length).include?(input.to_i)
-    user1.attacks(enemies[input.to_i])  
-  else puts "Petit malin"
-    combat_menu(*enemies)
+    user1.attacks(enemies[input.to_i])
+  else puts 'Petit malin'
+       combat_menu(*enemies)
   end
   enemies
 end
+
 human_battle
-
-
-
-
-
